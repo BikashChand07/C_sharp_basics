9e5ff4783cfc8966bcdab127de28308efd1778a1
BikashChand07 <bc5588264@gmail.com>
2024-03-01 15:10:03 +0545

    Class and objects

---
+++ 10_Class_and_Objects/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Book bk=new Book();
+          bk.setTitle("python Book");
+          bk.setauthor("Bikash Chand");
+          bk.setprice("100000");
+          bk.printBook(); 
+        }
+        
+    }
+    class Book
+    {
+        private string title;
+        private string price;
+        private string author;
+        public void setTitle(string tit)
+        {
+            title=tit;
+        }
+        public void setprice(string pric)
+        {
+            price=pric;
+        }
+        public void setauthor(string auth)
+        {
+            author=auth;
+        }
+        public void printBook()
+        {
+            Console.WriteLine("The title of the book: "+title+",written by "+author+" and co writer Mahesh joshi. The price of the book is $"+price);
+        }
+    }
+    
+}
